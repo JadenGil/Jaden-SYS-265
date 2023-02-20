875cbcef294c5eb1b91e5d08d61dbc0fb0700477
JadenGil <jaden.gilmond@mymail.champlain.edu>
2023-02-19 20:45:54 -0500

    newfile

---
+++ SYS-265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author jaden.gilmond
+#creates a new ssh user using $1 parameter
+#adds a public key from local repo or curled from remote repo
+#removes root ability to ssh in
+echo "ALL YOUR CODE GOES HERE
